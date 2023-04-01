2d817a71dde4fb53d1c4ae730b8db22f5e425450
Luiz-Felipe-SS <128910611+Luiz-Felipe-SS@users.noreply.github.com>
2023-03-31 21:40:09 -0300

    Minha Documentação SQL

---
+++ Minha_Documentação_SQL/Documentação.sql
@@ -0,0 +1,46 @@
+drop table pessoas;
+
+/*#Comando para a criação de tabelas. Ex: 'create table(...);'*/
+create table pessoas (
+id int not null auto_increment,
+nome varchar(50),
+nascimento date,
+sexo enum('M','F'),
+peso decimal(5,2),
+altura decimal(3,2),
+nascionalidade varchar(20) default 'Brasil',
+primary key(id)
+);
+
+/* #Comando para demonstrar as informações da tabela. */
+insert into pessoas (nome, nascimento, sexo, peso, altura) values 
+('luiz', '2000-2-13', 'M', 135.35, 1.90);
+
+/*#Comando para demostrar as informações da tabela.*/
+select * from pessoas;
+
+/*#comando para descrever todos os campos da tabela pessoas */
+desc pessoas;
+
+/*#Agora vamos alterar a tabela criada anteriormente e adicionar um novo campo chamado de PROFISSÃO, por padrão a adição de uma nova coluna acontece no */último campo.
+alter table pessoas add column profissao varchar(10);
+
+/*# um desc para visualizar*/
+desc pessoas;
+
+/*#depois de criar um novo campo na tabela, vamos deletar ele usando o comando drop column.*/
+alter table pessoas drop column profissao;
+
+
+/*# para adicionar uma coluna em uma determinada posição, preciso indicar o nocal usando o comando 'after'*/
+alter table pessoas add column profissao varchar(99) after nome ;
+
+/*#desc para visualizar*/
+desc pessoas;
+
+/*#caso você queira colocar o campo na primeira linha use o 'first'*/
+alter table pessoas add column codigo int first; 
+
+/*#agora vamos usar comandos para modificar os campos usando o comando 'modifh'
+aqui modifiquei o tamanho do varchar da coluna 'profissao' para o tamanho de 50*/
+alter table pessoas modify column profissao varchar(50);
